210ae373042c3a4252a7800bbbff5d42723673d2
uichior <uichior@gmail.com>
2025-06-09 10:47:28 +0900

    feat: サービス紹介のカルーセルコンポーネントを追加

--- components/ServicesCarousel.tsx
+++ components/ServicesCarousel.tsx
@@ -112,7 +112,6 @@ export function ServicesCarousel() {
         style={{
           scrollbarWidth: 'none',
           msOverflowStyle: 'none',
-          WebkitScrollbar: { display: 'none' }
         }}
       >
         {services.map((service, index) => {
